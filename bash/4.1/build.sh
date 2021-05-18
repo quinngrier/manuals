6ab79a1dbd858dd3ef9adcdb999969748d9658f9
Quinn Grier <quinn@quinngrier.com>
2021-05-17 21:59:15 -0700

    Add bash/4.1

---
+++ bash/4.1/build.sh
@@ -0,0 +1,26 @@
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
+
+set -e
+rm -f bash-4.1.tar.gz
+rm -f -r bash-4.1
+url=${1-'https://ftp.gnu.org/gnu/bash/bash-4.1.tar.gz'}
+wget -O bash-4.1.tar.gz "$url"
+sha256sum -c bash-4.1.tar.gz.sha256sum
+tar xf bash-4.1.tar.gz
+cd bash-4.1
+cp lib/readline/doc/hsuser.texi doc
+cp lib/readline/doc/rluser.texi doc
+cd doc
+texi2any --html --no-split bashref.texi
+cp bashref.html ../../index.html
+cd ../..
+rm -f bash-4.1.tar.gz
+rm -f -r bash-4.1
