dc3fb64fee3cced8d1d17f08252d7bf6a2ba4654
Quinn Grier <quinn@quinngrier.com>
2021-11-25 02:52:44 -0800

    Add the manuals.netlify.app mirror

---
+++ build-manuals.netlify.app.bash
@@ -0,0 +1,34 @@
+#! /bin/sh -
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
+
+set -e || exit $?
+. ./prelude.bash
+
+main() {
+
+  declare    d
+  declare    x
+  declare    xs
+
+  xs=$(git ls-files)
+  readonly xs
+
+  mkdir public
+  for x in $xs; do
+    x=./$x
+    d=public/${x%/*}
+    mkdir -p $d
+    cp $x $d
+  done
+
+}; readonly -f main
+
+main "$@"
