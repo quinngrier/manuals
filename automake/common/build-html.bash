c40fafb25e9dd4a5a6ddbe8c1f8a6d0d580f3e81
Quinn Grier <quinn@quinngrier.com>
2022-01-01 20:53:01 -0800

    Revise the automake build

--- automake/common/build-html.bash
+++ automake/common/build-html.bash
@@ -8,41 +8,89 @@
 # <https://creativecommons.org/publicdomain/zero/1.0/>.
 #
 
-set -E -e -u -o pipefail || exit $?
-trap exit ERR
+. /prelude.bash
 
-LC_ALL=C
-readonly LC_ALL
-export LC_ALL
+main() {
 
-mkdir out
-if test -f automake.texi; then
-  sed '
-    s/^@itemx \(....-..-.. Automake\)/@item \1/
-    s/Fran.ois Pinard/Fran@,{c}ois Pinard/g
-    s/Fran.ois'\''s/Fran@,{c}ois'\''s/g
-    /^@titlepage/,$ {
-      /^@ifinfo$/ d
-      /^@end ifinfo$/ d
-    }
-  ' automake.texi >tmp
-  mv -f tmp automake.texi
-  texi2any --html --no-split automake.texi
-  mv -f automake.html out
-else
-  cd doc
-  sed '
-    s/^@itemx \(....-..-.. Automake\)/@item \1/
-  ' automake.texi >tmp
-  mv -f tmp automake.texi
-  texi2any --html --no-split automake.texi
-  mv -f automake.html ../out
-  if test -f automake-history.texi; then
+  declare    d
+  declare -a outs
+  declare    x
+  declare    y
+
+  outs=()
+
+  #---------------------------------------------------------------------
+  # Patching
+  #---------------------------------------------------------------------
+
+  for x in ./**/*.texi; do
+
     sed '
       s/^@itemx \(....-..-.. Automake\)/@item \1/
-    ' automake-history.texi >tmp
-    mv -f tmp automake-history.texi
-    texi2any --html --no-split automake-history.texi
-    mv -f automake-history.html ../out
-  fi
-fi
+      s/Fran.ois Pinard/Fran@,{c}ois Pinard/g
+      s/Fran.ois'\''s/Fran@,{c}ois'\''s/g
+      /^@titlepage/,$ {
+        /^@ifinfo$/ d
+        /^@end ifinfo$/ d
+      }
+    ' "$x" >"$x.tmp"
+    mv "$x.tmp" "$x"
+
+  done
+
+  #---------------------------------------------------------------------
+  # Texinfo
+  #---------------------------------------------------------------------
+
+  for x in ./**/*.texi; do
+
+    y=$(
+      sed -n '
+        /^@titlepage/ {
+          p
+          q
+        }
+      ' "$x"
+    )
+    if [[ ! "$y" ]]; then
+      continue
+    fi
+
+    d=${x%/*}
+    x=${x##*/}
+
+    pushd "$d" >/dev/null
+
+    y=${x/%.texi/.html}
+
+    texi2any \
+      --html \
+      --no-split \
+      -o "$y" \
+      "$x" \
+    ;
+
+    outs+=("$d/$y")
+
+    popd >/dev/null
+
+  done
+
+  #---------------------------------------------------------------------
+
+  readonly outs
+
+  mkdir out
+  for x in ${outs[@]+"${outs[@]}"}; do
+    y=${x##*/}
+    if [[ -f "out/$y" ]]; then
+      barf "File already exists: \"out/$y\""
+    fi
+    mv "$x" out
+  done
+
+  #---------------------------------------------------------------------
+
+}; readonly -f main
+
+main "$@"
